137e9a89a9514aa9f60f1048e57511bc48f27155
Austin <austiny@snu.ac.kr>
2021-09-27 04:29:55 -0700

    Update testGit2.java
    v3

--- class0927/testGit2.java
+++ class0927/testGit2.java
@@ -2,6 +2,8 @@ package class0927;
 
 public class testGit2 {
     public static void main(String[] args) {
-        System.out.println("Hello Again!!!");
+        
+
+        System.out.println("TEST");
     }
 }
